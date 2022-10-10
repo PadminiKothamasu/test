23ecd0bf2d16a8dd24bd5454930070e906c9974c
PadminiKothamasu <PadminiKothamasu@gmail.com>
2022-10-10 21:35:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:275c11f4657c1418cd1a7b61569fe0868d838f6cdcd0f9accc2f5ebe1f43a668
+size 11327
---
+++ readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d1f6ec723a55c81ac67ed100dfe49845e85ed508abbcaeca7bf46aa34b59443
+size 1872
